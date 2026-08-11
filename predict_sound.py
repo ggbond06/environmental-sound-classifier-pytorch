dd3e840a537c1bb1cb14ad43d65da43bcf254221
ggbond06 <hu1266@outlook.com>
2026-08-11 18:53:22 +0200

    sperated the audio preprocessing function from predict_sound.py

--- predict_sound.py
+++ predict_sound.py
@@ -10,6 +10,8 @@ import pandas as pd
 
 import random
 
+from sound_model import SoundCNN
+
 to_spectrogram = nn.Sequential(
     torchaudio.transforms.MelSpectrogram(
         sample_rate=44_100,
@@ -41,36 +43,6 @@ def waveform_to_model_input(waveform):
     model_input = spectrogram.unsqueeze(0)
     return model_input
 
-class SoundCNN(nn.Module):
-    def __init__(self, input_shape:int, hidden_units:int, output_shape:int):
-        super().__init__()
-        self.block_1 = nn.Sequential(
-            nn.Conv2d(in_channels=input_shape,
-                      out_channels=hidden_units,
-                      kernel_size=3,
-                      stride=1,
-                      padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2,
-                         stride=2)
-        )
-        self.block_2 = nn.Sequential(
-            nn.Conv2d(in_channels=hidden_units,
-                      out_channels=hidden_units * 2,
-                      kernel_size=3,
-                      stride=1,
-                      padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2,
-                         stride=2),
-            nn.AdaptiveAvgPool2d(output_size=(1, 1)),
-            nn.Flatten(),
-            nn.Linear(in_features=hidden_units * 2,
-                      out_features=output_shape)
-        )
-
-    def forward(self, x):
-        return self.block_2(self.block_1(x))
 
 metadata_file_path = Path("data/environment_sound_dataset/ESC-50/meta/esc50.csv")
 
